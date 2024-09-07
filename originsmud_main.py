448eaabb1c8c38d04690d8be36faca562c6fa210
Jose Luis <joseluis.gomez@ingrammicro.com>
2024-09-07 13:14:16 +0200

    adding auth

--- originsmud_main.py
+++ originsmud_main.py
@@ -1,17 +1,29 @@
 import originsmud_server
 import db_connection
 import auth
+import logging
+import sys
+
+# Configuración de logs
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    stream=sys.stdout
+)
 
 if __name__ == "__main__":
-    print("Iniciando servidor OriginsMUD...")
+    logging.info("Iniciando servidor OriginsMUD...")
 
     # Comprobar la conexión a la base de datos
     connection = db_connection.connect_to_database()
     if connection:
-        print("Conexión a la base de datos establecida.")
+        logging.info("Conexión a la base de datos establecida.")
     else:
-        print("Error al conectar a la base de datos. Verifica las configuraciones.")
+        logging.error("Error al conectar a la base de datos. Verifica las configuraciones.")
         exit(1)  # Salir si no hay conexión a la base de datos
 
     # Iniciar el servidor y pasar la función de autenticación
+    logging.info("Iniciando el servidor...")
     originsmud_server.start_server(auth.authenticate_user)
+    logging.info("Servidor iniciado correctamente.")
